4ecbd8340d9e4dede252b7c2ea93a56016e830be
a <a@a.com>
2026-08-18 10:45:59 +0000

    feat: Added db.py and models.py into app dir

---
+++ app/db.py
@@ -0,0 +1,28 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "ecommerce")
+DATABASE_USER = os.getenv("DATABASE_USER", "ecommerce")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "ecommerce_password")
+
+
+DATABASE_URL = (
+    f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}"
+    f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+)
+
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+Base = declarative_base()
