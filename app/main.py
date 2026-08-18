72458a1afbebbaad926109ebab7b06fe5f31a94a
a <a@a.com>
2026-08-18 11:03:45 +0000

    feat: fix for building err

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 
+from .database import Base, engine
+from .models import Product
+
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="DevOps E-commerce API")
 
 
